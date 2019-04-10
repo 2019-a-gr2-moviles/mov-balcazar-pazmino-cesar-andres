0d58db8ecafb7925e124f4ca88abc819824db8a2
cabp1996 <41761406+cabp1996@users.noreply.github.com>
2019-04-10 15:56:47 -0500

    commit 2 nest

--- 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Post} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,38 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+//cuando se hace cambio en la logica del servidor se debe reiniciar el servidor
+  //para ello se puede usar npm run start:dev
+
+  @Post()//alt+enter para importar arriba directamente
+  postHello(){
+    return 'Hola mundo en post';
+  }
 }
+
+/*
+@nombreDecoradorClase //antes de instaciar clases, metodos, atributos. Un decorador es una función, Se ejecuta antes de crearse. DECORADPR->Funcion
+class clase{
+  @decoradorAtributo
+  public publico;
+  private privado;
+  protected protegido;
+
+  constructor(@parametro()publ,priv,prot){
+    this.publico=publ;
+    this.privado=priv;
+    this.protegido=prot;
+  }
+
+  @metodoA
+  public metodopublico(){
+
+  }
+
+    @metodob
+  private metodoprovado(){}
+
+    @metodoC //und decorador es una función que se ejecuta antes de algo(clases,parametros,metodoso,atributos)
+  protected metodoprotected(){}
+}*/
+
