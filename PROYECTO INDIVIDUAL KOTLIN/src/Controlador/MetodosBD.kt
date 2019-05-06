9c50046272d0fcc0f6ab28cb2f8aa2ee3cfeaad9
cabp1996 <andresbalcazar2020@gmail.com>
2019-05-05 20:59:46 -0500

    2do avance proyecto individual

--- PROYECTO INDIVIDUAL KOTLIN/src/Controlador/MetodosBD.kt
+++ PROYECTO INDIVIDUAL KOTLIN/src/Controlador/MetodosBD.kt
@@ -1,20 +1,47 @@
 package Controlador
+
 import Modelo.Instrumento;
 import Controlador.IOarchivos;
+import java.io.File
+import java.lang.Exception
+import javax.print.DocFlavor
 
-var nombreArchivo="Instrumento.txt"
+var nombreArchivo = "instrumento.txt"
+
 class MetodosBD {
 
 
     companion object {
-        var io=IOarchivos();
-        fun insertar(ins:Instrumento){
+        var io = IOarchivos();
+        fun insertar(id:Number,tipo:String,desc:String,marca:String,precio:Number) {
+            var ins=Instrumento(id,tipo,desc,marca,precio);
             io.escribirArchivo(ins.toString(), nombreArchivo);
         }
 
-        fun leer():List<String>{
-            var listaInstrumentos= io.readFileAsLinesUsingUseLines("src/basedatos"+ nombreArchivo);
-            return listaInstrumentos;
+        fun crearIndex(): Number {
+            var i = io.escribirArchivoIndex();
+            return i;
+        }
+
+        fun leer(): List<String> {
+            var listaInstrumentosMusicales:List<String> = emptyList()
+            try {
+                listaInstrumentosMusicales=io.readFileAsLinesUsingUseLines("src/basedatos/" + nombreArchivo);
+                return listaInstrumentosMusicales;
+            }catch(e:Exception){
+                return  listaInstrumentosMusicales;
+            }
+
+
+        }
+
+        fun eliminar(index: Number) {
+            io.eliminarTuplaArchivo(index);
+        }
+
+        fun actualizar(id:Number,tipo:String,desc: String,marca: String,precio: Number) {
+            var ins=Instrumento(id,tipo,desc,marca,precio);
+            io.actualizarTuplaArchivo(ins.toString());
         }
     }
 }
