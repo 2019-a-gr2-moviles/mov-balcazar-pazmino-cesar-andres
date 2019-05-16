635b2d3905acdc399f97323e7ff19b8cab048cee
cabp1996 <analy_chi>
2019-05-16 15:59:41 -0500

    Commit 1ER Intend

--- aplicacion2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ aplicacion2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
+    //las actividades tienen estado->tienen un ciclo de vida
+    //el oncreate es una de las etpas del ciclo de vida
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        btn_actividaddos.setOnClickListener {
+            iractdos()
+        }
+    }
+
+
+    fun iractdos(){
+        var intent=Intent(this,Actividad_dos::class.java)
+        startActivity(intent)
     }
 }
